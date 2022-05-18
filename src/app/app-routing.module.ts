7934e0d98cbd5c35852c77c58eb3495d75c5b7f8
Yorch1311 <jorgecam131199@outlook.com>
2022-05-17 20:54:22 -0600

    creation of  routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', children: [
+      { path: 'tournament', children: [
+          { path: '' },
+          { path: 'create' },
+          { path: 'edit/:id' },
+          { path: 'detail/:id' },
+          { path: 'manage-teams/:id' },
+        ],
+      },
+    ],
+  },
+  {path: 'trainer', children: [
+      { path: 'tournament/', children:[
+        { path: '' },
+        { path: ':id/categories' },
+        { path: ':id/categories/create-team' },
+      ]},
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
