3e2034e062607cbd64dfdd5fcee9ff44e3592cc3
Yorch1311 <jorgecam131199@outlook.com>
2022-05-18 15:36:03 -0600

    almonst finish tournament detail and tournament categories

--- src/app/modules/admins/services/type.ts
+++ src/app/modules/admins/services/type.ts
@@ -1,40 +1,47 @@
 //Estas son para el componente tournament detail
-export interface Chip{
-  id: number;
-  titulo: string;
-  estado: string;
-}
-export interface TournamentDetail {
+/*
+  id-chip
+  id-torneo
+  id-categoria
+  nombre-categoria
+  id-rama
+  nombre rama
 
+*/
+export interface TournamentDetail {
     name: string;
-    category: string[];
+    category: string;
     type: string;
-    places_detail: string[];
-    admin_detail: string[];
-    referee_detail: string[];
-    coach_detail: string[];
-    chips: Chip[];
+    places_detail: { name: string }[];
+    admin_detail: { name: string, apellido1: string }[];
+    referee_detail: { name: string, apellido1: string }[];
+    coach_detail: { name: string, apellido1: string }[];
+    chips: {
+      id_chip: string;
+      name_category: string;
+      name_branch: string;
+      status: string;
+    }[];
     dates: string;
     time: string;
-
 }
 
 //interface para recepcion de datos
-export interface Item {  
-  id: number;  
-  name: string;  
+export interface Item {
+  id: number;
+  name: string;
 }
 
 //interface para crear torneo
-export interface tupla {    
+export interface tupla {
   id_tournament?: number | any;
   name: string;
   category: number;
-  branch: number[]; 
+  branch: number[];
   type: number;
   town: number;
   places: number[];
-  dates: string;  
+  dates: string;
   time: string;
   administrators: number[];
   referees: number[];
