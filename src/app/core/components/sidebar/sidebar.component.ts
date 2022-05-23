b4a0ea9d297f66d38b2219f9e52d6738ba03ad3c
Yorch1311 <jorgecam131199@outlook.com>
2022-05-22 23:08:09 -0600

    creation of sidebar ended

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,7 +13,7 @@ export class SidebarComponent implements OnInit {
   referee: string | undefined;
   coach: string | undefined;
 
-  constructor(private location: Location) { 
+  constructor(private router: Router, private location: Location) { 
 
     if(this.location.path().includes("admin")){
       this.admin = "assets/image/icons/icons8-trofeo-48 2.png";
@@ -31,6 +32,30 @@ export class SidebarComponent implements OnInit {
       this.referee = "assets/image/icons/icons8-árbitro-48 1.png";
 
     }
+
+    this.router.events.subscribe(event => {
+      console.log("event on Router");
+
+      if(this.location.path().includes("admin")){
+        this.admin = "assets/image/icons/icons8-trofeo-48 2.png";
+        this.coach = "assets/image/icons/icons8-el-entrenador-lleno-48 1.png";
+        this.referee = "assets/image/icons/icons8-árbitro-48 1.png";
+  
+      }else if(this.location.path().includes("trainer")){
+        this.admin = "assets/image/icons/icons8-trofeo-48 1.png";
+        this.coach = "assets/image/icons/icons8-el-entrenador-lleno-48 2.png";
+        this.referee = "assets/image/icons/icons8-árbitro-48 1.png";
+  
+  
+      }else if(this.location.path().includes("referee")){
+        this.admin = "assets/image/icons/icons8-trofeo-48 1.png";
+        this.coach = "assets/image/icons/icons8-el-entrenador-lleno-48 1.png";
+        this.referee = "assets/image/icons/icons8-árbitro-48 1.png";
+  
+      }
+    })
+
+    
   }
 
   ngOnInit(): void {
